5caff0c95252b151466b3e8af436d4f2d4aa77fb
Koichi Akabe <vbkaisetsu@gmail.com>
2017-07-04 22:13:43 +0900

    Improve command-line interface

--- clopure.py
+++ clopure.py
@@ -4,24 +4,43 @@ import sys
 import readline
 import traceback
 
+from argparse import ArgumentParser
+
 from clopure.core import ClopureRunner
 from clopure.parser import ClopureParser
 from clopure.exceptions import ClopureSyntaxError, ClopureRuntimeError
 
 
 def main():
+
+    aparser = ArgumentParser()
+    aparser.add_argument("FILE", nargs="?", default="", help="Script file")
+    aparser.add_argument("-t", "--threads", type=int, default=1, help="Number of threads")
+    aparser.add_argument("-q", "--queues", type=int, default=100, help="Number of queues")
+    args = aparser.parse_args()
+
     clparser = ClopureParser()
-    runner = ClopureRunner()
+    runner = ClopureRunner(threads=args.threads, queue_size=args.queues)
+
+    if args.FILE:
+        stream = open(args.FILE, "r")
+    else:
+        stream = sys.stdin
 
     while True:
-        try:
-            if clparser.is_empty():
-                line = input(">>> ").rstrip()
-            else:
-                line = input("... ").rstrip()
-        except EOFError:
-            print()
-            return
+        if not args.FILE and sys.stdin.isatty():
+            try:
+                if clparser.is_empty():
+                    line = input(">>> ").rstrip()
+                else:
+                    line = input("... ").rstrip()
+            except EOFError:
+                print()
+                break
+        else:
+            line = stream.readline()
+            if not line:
+                break
         try:
             trees = clparser.parse_line(line)
         except ClopureSyntaxError as e:
@@ -33,11 +52,14 @@ def main():
         try:
             for tree in trees:
                 result = runner.evaluate(tree)
-                print(result)
+                if not args.FILE and sys.stdin.isatty():
+                    print(result)
         except ClopureRuntimeError as e:
             print("ClopureRuntimeError: %s" % str(e), file=sys.stdout)
         except Exception:
             traceback.print_exc(file=sys.stdout)
+    if not clparser.is_empty():
+        print("Syntax error: Form is not closed", file=sys.stdout)
 
 
 if __name__ == "__main__":
