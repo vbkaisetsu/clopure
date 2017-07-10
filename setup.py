3d7781c6b3f2314945b3fbb303bbeacf98cb4228
Koichi Akabe <vbkaisetsu@gmail.com>
2017-07-10 22:50:02 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup
 
 setup(
     name = "clopure",
-    version = "0.1",
+    version = "0.1.1",
     description = "Multi-processing supported functional language",
     url = "https://github.com/vbkaisetsu/clopure",
     author = "Koichi Akabe",
     author_email = "vbkaisetsu@gmail.com",
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha"
+        "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.6",
