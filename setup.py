fe1119e08491836d03b1d0976d717d80c35554b9
Koichi Akabe <vbkaisetsu@gmail.com>
2017-07-10 20:42:23 +0900

    Update description and classifiers

--- setup.py
+++ setup.py
@@ -6,10 +6,17 @@ from setuptools import setup
 setup(
     name = "clopure",
     version = "0.1",
-    description = "Multi-processing supported functional language for Python",
+    description = "Multi-processing supported functional language",
     url = "https://github.com/vbkaisetsu/clopure",
     author = "Koichi Akabe",
     author_email = "vbkaisetsu@gmail.com",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha"
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Interpreters",
+    ],
     license = "MIT",
     packages = ["clopure"],
     test_suite = "test",
